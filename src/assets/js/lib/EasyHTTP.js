7620cce5f0548ed00e05dbd4d03c0bf57d917942
Nika Sepiskveradze <sebiskver@gmail.com>
2019-01-04 23:58:48 +0400

    Added Simple Custom HTTP Library For Request Handling

---
+++ src/assets/js/lib/EasyHTTP.js
@@ -0,0 +1,61 @@
+class EasyHTTP {
+  constructor() {
+    this.http = new XMLHttpRequest();
+  }
+
+  // Make an GET Request
+  get(url, callback) {
+    this.http.open('GET', url, true);
+
+    this.http.onload = () => {
+      if(this.http.status === 200) {
+        callback(null, this.http.responseText);
+      }else {
+        callback('Error: ' + this.http.status);
+      }
+    }
+
+    this.http.send();
+  }
+
+  // Make an POST Request
+  post(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-Type', 'application/json');
+
+    this.http.onload = () => {
+      callback(null, this.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+  }
+
+  // Make an PUT Request
+  put(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-Type', 'application/json');
+
+    this.http.onload = () => {
+      callback(null, this.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+  }
+
+  // Make an DELETE Request
+  delete(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    this.http.onload = () => {
+      if(this.http.status === 200) {
+        callback(null, 'Deleted...');
+      }else {
+        callback('Error: ' + this.http.status);
+      }
+    }
+
+    this.http.send();
+  }
+}
+
+export default EasyHTTP;
